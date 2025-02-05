5e148c4867c36adcb4a93ccb4020bc997d55ee04
Az00mi <annabogdanova498@gmail.com>
2025-02-05 22:33:34 +0200

    Створено початковий шаблон сторінки

---
+++ src/components/description/Description.jsx
@@ -0,0 +1,9 @@
+import "./Description.css"
+
+export default function Description(){
+    return(
+        <div className="description">
+            Тут буде назва та опис проєкту, а також трошки про автора :)
+        </div>
+    )
+}
---
+++ src/components/mainPart/MainPart.jsx
@@ -0,0 +1,10 @@
+import "./MainPart.css"
+import Description from "../description/Description.jsx";
+export default function MainPart(){
+    return(
+        <main>
+            <Description/>
+            Робоча область
+        </main>
+    )
+}
